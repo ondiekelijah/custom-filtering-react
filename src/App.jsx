e32fba2843aec4e8bf5dc22285c206ef7af90838
Dev-Elie <eochieng9448@gmail.com>
2022-10-29 21:32:37 +0300

    feat: uncheck toggle during handleReset

--- src/App.jsx
+++ src/App.jsx
@@ -96,7 +96,10 @@ function App() {
     setSearchBarVal("");
     setDropCityVal("");
     setDropPriceVal("");
-    // setToggleState(true);
+    // get toggle by classname and uncheck it
+    const toggle = document.querySelector(".toggle");
+    toggle.checked = false;
+    setToggleState(false);
   };
 
   // Sorting
@@ -208,7 +211,7 @@ function App() {
           <input
             type="checkbox"
             id="default-toggle"
-            className="sr-only peer"
+            className="sr-only peer toggle"
             onClick={handleToggle}  
           />
           <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600" />
