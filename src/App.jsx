3083e58b15858ea22a129e1d42b11e43baccf6eb
Dev-Elie <eochieng9448@gmail.com>
2022-10-29 14:51:34 +0300

    chore: tidy up

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ function App() {
   const [events, setEvents] = useState([]);
   const [searchBarVal, setSearchBarVal] = useState("");
   const [order, setOrder] = useState("asc");
-  const [toggleState, setToggleState] = useState(true);
+  const [toggleState, setToggleState] = useState(false);
   const [dropCityVal, setDropCityVal] = useState("");
   const [dropPriceVal, setDropPriceVal] = useState("");
 
-  // find all cities for the select
+  // find all cities for the select menu
   const cities = [...new Set(Events.map((event) => event.city))];
 
-  // Create values for display to show the price ranges, example $0 - $20 uniformly
+  // Values for display to show the price ranges, example $0 - $20 uniformly
   const priceRanges = [
     { value: "0-50", label: "$0 - $50" },
     { value: "50-100", label: "$50-$100" },
@@ -72,7 +72,7 @@ function App() {
 
   // Handle search via toggle
   const handleToggle = (e) => {
-    if (toggleState === true) {
+    if (toggleState === false) {
       // show all events with the lowest price out of all the events
       const filteredEvents = Events.filter((event) => {
         return (
@@ -80,10 +80,11 @@ function App() {
         );
       });
       setEvents(filteredEvents);
-      setToggleState(false);
-    } else {
-      setEvents(Events);
       setToggleState(true);
+    } else {
+      // show all events
+      setEvents(Events);
+      setToggleState(false)
     }
   };
 
@@ -92,6 +93,7 @@ function App() {
     setSearchBarVal("");
     setDropCityVal("");
     setDropPriceVal("");
+    // setToggleState(true);
   };
 
   const handleCityOrder = (col) => {
@@ -199,10 +201,9 @@ function App() {
         >
           <input
             type="checkbox"
-            defaultValue
             id="default-toggle"
             className="sr-only peer"
-            onClick={handleToggle}
+            onClick={handleToggle}  
           />
           <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600" />
           <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
