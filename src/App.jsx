3c5765e2660e9239e23b3a05735a21f7ce3700c7
Dev-Elie <eochieng9448@gmail.com>
2022-10-30 09:55:38 +0300

    fix: filter buttons padding

--- src/App.jsx
+++ src/App.jsx
@@ -177,7 +177,7 @@ function App() {
           className="bg-gray-50 border border-gray-300 
           text-gray-900 text-sm rounded-lg focus:ring-blue-500
            focus:border-blue-500 block 
-           md:w-[20%] w-[full] p-2.5 dark:bg-gray-700 
+           md:w-[20%] w-[full] p-2 dark:bg-gray-700 
            dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
            dark:focus:ring-blue-500 dark:focus:border-blue-500
             md:mt-0 mt-2"
@@ -195,7 +195,7 @@ function App() {
           onClick={handleFilters}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 
           focus:border-blue-500 block 
-          md:w-[20%] w-[full] p-2.5 dark:bg-gray-700 
+          md:w-[20%] w-[full] p-2 dark:bg-gray-700 
           dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
           dark:focus:ring-blue-500 dark:focus:border-blue-500
            md:mt-0 mt-2"
